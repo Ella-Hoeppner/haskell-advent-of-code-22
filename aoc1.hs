65f2f5474395fbb880d29668c151dbe2ea34b862
Ella Hoeppner <ellahoeppner@gmail.com>
2023-08-18 14:22:26 -0400

    initial commit, day 1 part 1

---
+++ aoc1.hs
@@ -0,0 +1,22 @@
+splitStringRec :: [String] -> String -> String -> [String]
+splitStringRec splitStrings "" _ = splitStrings
+splitStringRec splitStrings s splitter = 
+  if splitter == (take (length splitter) s)
+    then splitStringRec (splitStrings ++ [""]) 
+                        (drop (length splitter) s)
+                        splitter
+    else splitStringRec ((init splitStrings) ++ [(last splitStrings) ++ [(head s)]])
+                        (tail s)
+                        splitter
+
+splitString :: String -> String -> [String]
+splitString = splitStringRec [""]
+
+countCalories :: String -> [Int]
+countCalories calorieString = 
+  [sum [read calorieString :: Int | calorieString <- splitString items "\n", not (null calorieString)] | items <- splitString calorieString "\n\n"]
+
+maxCalories :: String -> Int
+maxCalories = maximum . countCalories
+
+--maxCalories "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000\n"
