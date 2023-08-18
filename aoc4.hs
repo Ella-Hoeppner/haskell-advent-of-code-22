0c219eb545ad7ab9912814df4fbb76c92993ae8e
Ella Hoeppner <ellahoeppner@gmail.com>
2023-08-18 15:40:27 -0400

    day 4 part 1

---
+++ aoc4.hs
@@ -0,0 +1,31 @@
+import Data.Char (ord)
+
+splitStringRec :: [String] -> String -> String -> [String]
+splitStringRec splitStrings "" _ = splitStrings
+splitStringRec splitStrings s splitter = 
+  if splitter == (take (length splitter) s)
+    then splitStringRec (splitStrings ++ [""]) 
+                        (drop (length splitter) s)
+                        splitter
+    else splitStringRec ((init splitStrings) ++ [(last splitStrings) ++ [(head s)]])
+                        (tail s)
+                        splitter
+
+splitString :: String -> String -> [String]
+splitString = splitStringRec [""]
+
+rangeContains [[min1, max1], [min2, max2]] =
+  min1 >= min2 && max1 <= max2
+
+eitherRangeContains ranges
+  = rangeContains ranges || rangeContains (reverse ranges)
+
+elfRangesStringInvalid s = 
+  eitherRangeContains
+    [[read bound :: Int | bound <- splitString rangeString "-"]
+       | rangeString <- splitString s ","]
+
+invalidElfRangesCount s = 
+  sum [1 | line <- splitString s "\n", elfRangesStringInvalid line]
+
+-- invalidElfRangesCount "2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8"
